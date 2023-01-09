693df091079d5a99a90a162f5845d76ee2c8d3e1
Douglas Deodanes <deodanes.dbz@gmail.com>
2023-01-09 02:21:38 -0600

    solved jsonwebtoken auth

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -5,29 +5,83 @@ const { bcrypt, compare } = require('../utils/handlePassword')
 const { tokenSign } = require('../utils/handleJwt')
 
 const signup = async (req, res) => {
+    try {
+        req = matchedData(req)
+        const password = await bcrypt(req.password)
+        const body = { ...req, password }
 
-    req = matchedData(req)
-    const password = await bcrypt(req.password)
-    const body = { ...req, password }
+        const dataUser = await Auth.create(body)
+        dataUser.set('password', undefined, { strict: false })
 
-    const dataUser = await Auth.create(body)
-    dataUser.set('password', undefined, { strict: false })
+        const data = {
 
-    const data = {
+            token: await tokenSign(dataUser),
+            user: dataUser
 
-        token: await tokenSign(dataUser),
-        user: dataUser
+        }
+
+        res.send({ data })
+
+    } catch (error) {
+
+        res.status(500).json({
+            status: 500,
+            data: user,
+            message: 'ha ocurrido un error en el servidor'
+        })
 
     }
-
-    res.send({ data })
 
 }
 
 const signin = async (req, res = response) => {
-    res.json('signin')
+
+    try {
+
+        req = matchedData(req)
+        const user = await Auth.findOne({ username: req.username })
+            .select('name lastname password email role')
+
+        if (!user) {
+
+            return res.status(404).json({
+                status: 404,
+                data: user,
+                message: 'user not found'
+            })
+        }
+
+        // const hashPassword = user.password;
+
+        const hashPassword = user.get('password');
+        const check = await compare(req.password, hashPassword)
+
+        if (!check) {
+            return res.status(401).json({
+                status: 401,
+                message: 'password incorrect'
+            })
+        }
+
+        user.set('password', undefined, { strict: false })
+        const data = {
+            token: await tokenSign(user),
+            user
+        }
+
+        res.send({ data })
+
+    } catch (error) {
+
+        return res.status(500).json({
+            stastus: 500,
+            data: user,
+            message: 'Upps ha ocurrido un error en el servidor'
+        })
+    }
 
 }
+
 const profile = async (req, res = response) => {
     res.json('profile')
 
